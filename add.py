db831eb7dbcaabc7cf153176996a99421c582daa
ABISHEK K BIJU <10217@fisat>
2019-05-15 04:56:12 -0400

    create a function to add four nubers

--- add.py
+++ add.py
@@ -1,8 +1,12 @@
 def add2(n1,n2):
 	s=n1+n2
 	return s
-def add2(n1+n2+n3)
+def add3(n1+n2+n3)
 	s=n1+n2+n3
+	return s
+def add4(n1+n2+n3+,n4)
+	s=n1+n2+n3+n4
 	return s
 print add2(2,3)
 print add3(1,2,3)
+print add4(2,3,4,5)
